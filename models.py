f27b3c85d0d12ff154f7d96e470b8e556b9c967b
Arash Ansari <a.ansari@duets.ai>
2023-12-01 14:06:40 -0800

    Update user model to add hashed_password and reflect different auth stages

--- models.py
+++ models.py
@@ -4,11 +4,14 @@ from django.db import models
 
 class User(models.Model):
     uuid = models.CharField(max_length=128, unique=True, default=uuid.uuid4, primary_key=True)
+    email = models.EmailField(max_length=50)
+
     native_language = models.CharField(max_length=50, blank=True)
     target_language = models.CharField(max_length=50, blank=True)
-    email = models.EmailField(max_length=50)
-    full_name = models.CharField(max_length=50)
     is_teacher = models.BooleanField(default=False)
+    
+
+    full_name = models.CharField(max_length=50)
     zoom_access_token = models.CharField(max_length=800)
     zoom_refresh_token = models.CharField(max_length=800)
     zoom_token_expiration = models.CharField(max_length=512)
